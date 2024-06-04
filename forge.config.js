5802560faa7820eb0708f58b1093e61531afc5db
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-06-04 16:33:55 +0500

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -7,8 +7,8 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'github-user-name',
-          name: 'github-repo-name'
+          owner: 'MuhammadNomanShoukat',
+          name: 'electron-version-update'
         },
         prerelease: false,
         draft: true
