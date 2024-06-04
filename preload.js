9f5b26655838ff53e7cceeeb26356a266923c2df
IT <it@Liveadmins.com>
2024-06-04 14:47:02 +0500

    code added

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron")
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    simpleCall: ()=>"Hello renderer...",
-    renderToMainOneWay: (value)=>ipcRenderer.send("setWindowlable", value),
-    renderToMainTwoWay: ()=>ipcRenderer.invoke("open:dailog"),
-    counterHandler: (callback)=>ipcRenderer.on("counter-handler", (_event, data)=>callback(data)),
-    counterValue: (value)=>ipcRenderer.send("counter-value", value)
-})
